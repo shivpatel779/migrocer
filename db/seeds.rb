38bbd3a40b213bae96ff42008c9c7d2109c0d258
sandy <sandeepsinghsisodiya98@gmail.com>
2017-05-30 19:05:57 +0530

    update auth error

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,5 +14,5 @@ Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
 if Rails.env.eql?("development")
   Spree::AuthenticationMethod.create(environment: "development", provider: "facebook", api_key: "311692455936768", api_secret: "b01173a5069169f47de2d409719af2ee", active: true)
 else
-  Spree::AuthenticationMethod.create(environment: "development", provider: "facebook", api_key: "311692455936768", api_secret: "b01173a5069169f47de2d409719af2ee", active: true)
+  Spree::AuthenticationMethod.create(environment: "production", provider: "facebook", api_key: "311692455936768", api_secret: "b01173a5069169f47de2d409719af2ee", active: true)
 end
